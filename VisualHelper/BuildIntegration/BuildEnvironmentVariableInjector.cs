ff1b9b4f198849b187ffa383f7ccf32961c02323
adickin <adamdickin@gmail.com>
2017-09-17 13:43:41 -0600

    upgrade build number, figure out how to set environment variables for the editor after it starts.

---
+++ VisualHelper/BuildIntegration/BuildEnvironmentVariableInjector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualHelper.BuildIntegration
+{
+   public class BuildEnvironmentVariableInjector
+   {
+
+      public const string VISUAL_HELPER = "VISUAL_HELPER";
+
+      /*
+       * all that is needed to inject env variables into the build is to just set them once.
+       * Should build this to have profiles of environment variables.
+       */
+      public BuildEnvironmentVariableInjector()
+      {
+
+         Environment.SetEnvironmentVariable("VISUAL_HELPER", @"D:\workspace\shairport4w\visualHelperTest_canBeDeleted");
+
+      }
+
+   }
+}
